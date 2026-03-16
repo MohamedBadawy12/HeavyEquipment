1af06847a6c51c9cb188c99d56814303566f28e1
MohamedBadawy12 <m.badawy2442002@gmail.com>
2026-03-16 03:24:34 +0200

    Adding NotficationController and View

---
+++ HeavyEquipment.WebMVC/Controllers/NotificationController.cs
@@ -0,0 +1,32 @@
+﻿using HeavyEquipment.Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace HeavyEquipment.WebMVC.Controllers
+{
+    public class NotificationController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public NotificationController(IUnitOfWork unitOfWork)
+            => _unitOfWork = unitOfWork;
+
+        private Guid CurrentUserId =>
+            Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
+        public async Task<IActionResult> Index()
+        {
+            var notifications = await _unitOfWork.Notifications
+                .GetUnreadByUserIdAsync(CurrentUserId);
+            return View(notifications);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> MarkAllAsRead()
+        {
+            await _unitOfWork.Notifications.MarkAllAsReadAsync(CurrentUserId);
+            TempData["Success"] = "تم تحديد كل الإشعارات كمقروءة";
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
